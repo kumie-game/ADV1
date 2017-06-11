95343e67f30dc8f0b0c43d45b5f84bf90220f205
t.ogawa <tomozo.oga.oga@gmail.com>
2017-06-11 18:40:45 +0900

    最初のコミットです

---
+++ New Unity Project/Assets/CmaeraControiller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CmaeraControiller : MonoBehaviour {
+
+	GameObject player;
+	void Start () {
+		this.player = GameObject.Find ("ThirdPersonController");
+		
+	}
+
+	void Update () {
+		Vector3 playerPos = this.player.transform.position;
+		transform.position = new Vector3 (
+			playerPos.x, playerPos.y+15, playerPos.z-20);
+
+		/*Vector3 playerRot = this.player.transform.rotation;
+
+		transform.rotation = new Vector3 (
+			playerRot.x-10, playerPos.y, playerPos.z);*/
+	
+		
+	}
+}
